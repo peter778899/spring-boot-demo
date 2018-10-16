10d030b760549c628387beea2f89549511db2982
潘天峰 <pantianfeng@pantianengdeMBP.lan>
2018-10-16 22:58:40 +0800

    nothing

--- aop-demo/src/main/java/com/tx/stu/Application.java
+++ aop-demo/src/main/java/com/tx/stu/Application.java
@@ -8,7 +8,6 @@ import java.util.Date;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        //test
         SpringApplication.run(Application.class, args);
     }
 }
