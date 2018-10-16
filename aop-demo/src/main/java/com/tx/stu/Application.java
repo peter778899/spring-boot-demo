4bfd4f73413ec961321d70f34103e3391b99fc2e
潘天峰 <pantianfeng@pantianengdeMBP.lan>
2018-10-16 22:52:52 +0800

    test

--- aop-demo/src/main/java/com/tx/stu/Application.java
+++ aop-demo/src/main/java/com/tx/stu/Application.java
@@ -3,9 +3,12 @@ package com.tx.stu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        //test
         SpringApplication.run(Application.class, args);
     }
 }
